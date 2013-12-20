40c8ce5033ed69567616d631847f64f2e2182e8f
codeseven58 <undyingvisage@hotmail.com>
2013-12-19 20:49:23 -0800

    Original upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+void paktc();
+
+int main()
+{
+    cout << "well that was really fucking annoying" << endl;
+    paktc();
+    return 0;
+}
+
+void paktc()
+{
+    cout << "Press any key to continue." << endl;
+    getch();
+}
+
